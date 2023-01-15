b542a5ed738c64bb673974d617f7a1aed88b7f21
amin.mokhtari94@gmail.com <amin.mokhtari94@gmail.com>
2023-01-15 10:58:43 +0330

    fix auth

--- install.sh
+++ install.sh
@@ -12,6 +12,12 @@ function auth() {
   read -p "Enter username: " username
   read -p "Enter password: " password
   docker run --rm -ti xmartlabs/htpasswd $username $password >nginx/conf/nginx.htpasswd
+  echo 'AUTH_BASIC="Registry realm"' >.env
+}
+
+function noauth() {
+  echo 'AUTH_BASIC="off"' >.env
+  echo "" >nginx/conf/nginx.htpasswd
 }
 
 function run_ssl() {
@@ -20,10 +26,10 @@ function run_ssl() {
   exit 0
 }
 
-read -p "Enable auth credentials? (y/N): " auth && [[ $auth == [yY] || $auth == [yY][eE][sS] ]] && auth || echo "" >nginx/conf/nginx.htpasswd
+read -p "Enable auth credentials? (y/N): " auth && [[ $auth == [yY] || $auth == [yY][eE][sS] ]] && auth || noauth
 read -p "Enable https? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || run
 
-echo "Starting to requesting a certificate for *.$domain.ir"
+echo "Starting to requesting a certificate for *.$domain"
 read -p "Enter your email: " email
 
 docker-compose run certbot certonly --preferred-challenges=dns --agree-tos --email $email --manual -d *.$domain --server https://acme-v02.api.letsencrypt.org/directory && run_ssl || exit 1
